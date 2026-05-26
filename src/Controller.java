996da59de1d35c025b122f82cc65e8bc60881bd6
ReneChamorro <rchamorrocrugeiras@danielcastelao.org>
2026-05-26 09:31:43 +0200

    Clases añadidas

--- src/Controller.java
+++ src/Controller.java
@@ -1,27 +1,47 @@
 import java.lang.module.ModuleDescriptor;
 
 public class Controller {
+    static View miView = new View();
+    Model miModel = new Model();
+
     public static void main(String[] args) {
-        // Instanciamos la vista y el modelo
-        View miView = new View();
-        Model miModel = new Model();
+        miView.menu();
 
-        // Crear tres coches
-        miModel.crearCoche("LaFerrari", "SBC 1234");
-        miModel.crearCoche("Alpine", "HYU 4567");
-        miModel.crearCoche("Aston Martin", "FGH 3333");
+        /**
+         *
 
-        Coche ferrari = miModel.getCoche("SBC 1234");
-        // modifica la velocidad
-        int nuevaVelocidad = miModel.cambiarVelocidad("SBC 1234", 30);
+         // Instanciamos la vista y el modelo
 
-        // recoje la velocidad y la muestra (tarea de la View)
-        boolean hecho = miView.muestraVelocidad("SBC 1234", miModel.getVelocidad("SBC 1234"));
+         // Crear tres coches
+         miModel.crearCoche("LaFerrari", "SBC 1234");
+         miModel.crearCoche("Alpine", "HYU 4567");
+         miModel.crearCoche("Aston Martin", "FGH 3333");
 
-        if (hecho) {
-            System.out.println("[LOG] Correcto");
-        } else {
-            System.out.println("[LOG] Error");
-        } ;
+         Coche ferrari = miModel.getCoche("SBC 1234");
+         // modifica la velocidad
+         int nuevaVelocidad = miModel.cambiarVelocidad("SBC 1234", 30);
+
+         // recoje la velocidad y la muestra (tarea de la View)
+         boolean hecho = miView.muestraVelocidad("SBC 1234", miModel.getVelocidad("SBC 1234"));
+
+
+         if (hecho) {
+         System.out.println("[LOG] Correcto");
+         } else {
+         System.out.println("[LOG] Error");
+         } ; **/
+    }
+
+    public boolean añadirCoche(String modelo, String matricula){
+        Coche creado =miModel.crearCoche(modelo,matricula);
+        if (creado == miModel.getCoche(matricula)) {
+            return true;}
+        else {return false;}
+    }
+
+    public boolean newVelocidad(String matricula, int velocidad){
+        int nuevaVelocidad = miModel.cambiarVelocidad(matricula, velocidad);
+        boolean hecho = miView.muestraVelocidad(matricula, miModel.getVelocidad(matricula));
+        return hecho;
     }
 }
--- src/View.java
+++ src/View.java
@@ -1,15 +1,55 @@
+import java.util.Scanner;
+
 /**
  * Clase encargada de la interacción con el usuario
  */
 public class View {
-    /**
-     * Muestra la velocidad de un coche
-     * @param matricula del coche
-     * @param v velocidad
-     * @return true si se ha mostrado correctamente
-     */
+
+
+    public void menu() {
+        Controller c = new Controller();
+        int opcion = 0;
+        do {
+            System.out.println("\n--- XESTOR COCHES ---");
+            System.out.println("1. Engadir Coche");
+            System.out.println("2. Cambiar velocidad");
+            System.out.println("3. Salir");
+            System.out.print("Selecciona unha opción: ");
+
+            opcion = new Scanner(System.in).nextInt();
+            switch (opcion) {
+                case 1 -> {
+                    System.out.println("Modelo: ");
+                    String modelo = new Scanner(System.in).next();
+                    System.out.println("Matricula: ");
+                    String matricula = new Scanner(System.in).next();
+                    boolean a = c.añadirCoche(modelo, matricula);
+                    if (a == true) {
+                        System.out.println("Se ha añadido correctamente");}
+                    else {System.out.println("Algo ha fallado");}
+                }
+
+                case 2 -> {
+                    System.out.println("Matricula: ");
+                    String matricula = new Scanner(System.in).next();
+                    System.out.println("Nueva velocidad: ");
+                    int velocidad = new Scanner(System.in).nextInt();
+                    boolean a =c.newVelocidad(matricula,velocidad);
+                    if (a == true) {
+                        System.out.println("Se ha cambiado correctamente");}
+                    else {System.out.println("Algo ha fallado");}
+                }
+
+                case 3 -> {
+                    System.out.println("Chao hasta luego....");
+                }
+            }
+        } while (opcion != 3);
+    }
+
+
     public boolean muestraVelocidad(String matricula, Integer v){
-        System.out.println(matricula + ": " + v + "km/hr");
+        System.out.println(matricula + ": " + v + "km/h");
         return true;
     }
 }
