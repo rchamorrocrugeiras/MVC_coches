3213fa6671082d318ec994711427ad3c7d845d33
ReneChamorro <rchamorrocrugeiras@danielcastelao.org>
2026-05-26 09:39:51 +0200

    Modifico la clase View

--- src/View.java
+++ src/View.java
@@ -13,7 +13,8 @@ public class View {
             System.out.println("\n--- XESTOR COCHES ---");
             System.out.println("1. Engadir Coche");
             System.out.println("2. Cambiar velocidad");
-            System.out.println("3. Salir");
+            System.out.println("3. Avanzar");
+            System.out.println("4. Salir");
             System.out.print("Selecciona unha opción: ");
 
             opcion = new Scanner(System.in).nextInt();
@@ -25,8 +26,10 @@ public class View {
                     String matricula = new Scanner(System.in).next();
                     boolean a = c.añadirCoche(modelo, matricula);
                     if (a == true) {
-                        System.out.println("Se ha añadido correctamente");}
-                    else {System.out.println("Algo ha fallado");}
+                        System.out.println("Se ha añadido correctamente");
+                    } else {
+                        System.out.println("Algo ha fallado");
+                    }
                 }
 
                 case 2 -> {
@@ -34,22 +37,43 @@ public class View {
                     String matricula = new Scanner(System.in).next();
                     System.out.println("Nueva velocidad: ");
                     int velocidad = new Scanner(System.in).nextInt();
-                    boolean a =c.newVelocidad(matricula,velocidad);
+                    boolean a = c.newVelocidad(matricula, velocidad);
                     if (a == true) {
-                        System.out.println("Se ha cambiado correctamente");}
-                    else {System.out.println("Algo ha fallado");}
+                        System.out.println("Se ha cambiado correctamente");
+                    } else {
+                        System.out.println("Algo ha fallado");
+                    }
                 }
 
                 case 3 -> {
-                    System.out.println("Chao hasta luego....");
+                    System.out.println("Matricula: ");
+                    String matricula = new Scanner(System.in).next();
+
+                    System.out.println("Metros a avanzar: ");
+                    int metros = new Scanner(System.in).nextInt();
+
+                    boolean a = c.avanzarCoche(matricula, metros);
+
+                    if (a == true) {
+                        System.out.println("El coche avanzó correctamente");
+                    } else {
+                        System.out.println("Algo ha fallado");
+                    }
                 }
             }
-        } while (opcion != 3);
+        } while (opcion != 4);
     }
 
 
-    public boolean muestraVelocidad(String matricula, Integer v){
+    public boolean muestraVelocidad(String matricula, Integer v) {
         System.out.println(matricula + ": " + v + "km/h");
+        return true;
+    }
+
+    public boolean mostrarMetros(String matricula, Integer metros){
+
+        System.out.println(matricula + ": " + metros + " metros recorridos");
+
         return true;
     }
 }
