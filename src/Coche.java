3ed56a66ee7e773f5cf98e47467ada4a39079937
ReneChamorro <rchamorrocrugeiras@danielcastelao.org>
2026-05-26 09:36:45 +0200

    Modifico la clase Coche

--- src/Coche.java
+++ src/Coche.java
@@ -2,10 +2,12 @@ public class Coche {
     String matricula;
     String modelo;
     Integer velocidad;
+    Integer metrosRecorridos;
 
     public Coche(String modelo, String matricula) {
         this.modelo = modelo;
         this.matricula = matricula;
         this.velocidad = 0;
+        this.metrosRecorridos = 0;
     }
 }
